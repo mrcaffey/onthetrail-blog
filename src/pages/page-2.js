f00b11887e86861d172b494fad4f1fa5ec4d2ba3
Micah Caffey <mrcaffey@gmail.com>
2019-01-19 15:31:36 -0700

    deployment steps

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -8,27 +8,18 @@ const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
     <h1>Welcome to My Portfolio!</h1>
-    <br/>
-    <br/>
       <h2>Enterprise International Web Application</h2>
-      <img className="enterprise-image" alt="enterprise" src={Enterprise} />
-      <p><b>Technologies Used:</b><br/>
-      </br>ReactJS, Ruby, Rails Framework</p>
-      <p><b>Description:</b><br /></p>
+      <p>Technologies Used:
+            ReactJS, Ruby, Rails Framework
+      </p>
+      <p>Description:</p>
       <p>I was part of a team in the University of Utah's Web Development program
-      designing a travel app aiding Asian clientele to travel arrangements to see American tourist destinations.
-      My role in this project was controller testing, admin dashboard/user login creation using ReactJS, pulling in data to retrieve users, 
-      and assisting in setting up Models, Views and Controllers for the Rails portion of the app.</p>
+        designing a travel app aiding Asian clientele to travel arrangements to see American tourist destinations.
+        My role in this project was controller testing, admin dashboard/user login creation using ReactJS, pulling in data to retrieve users, 
+        and assisting in setting up Models, Views and Controllers for the Rails portion of the app.
+      </p>
       <Link to="https://dps-enterprise-international.herokuapp.com/">Enterprise International</Link>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
+
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
