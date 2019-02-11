1edeaf1f310260719811af71d37a96a0e45b68cf
Micah Caffey <mrcaffey@gmail.com>
2019-02-10 19:19:31 -0700

    changes

--- src/pages/my-firstpost/index.md
+++ src/pages/my-firstpost/index.md
@@ -2,7 +2,7 @@
 title: "My First Blog Post"
 description: A queried blog post
 date: '2019-02-08'
-image: ' '
+image: ./'januaryhike_40.jpg'
 ---
 
 
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,23 +1,23 @@
 
 import React from 'react';
 import Layout from '../components/layout';
+import Img from 'gatsby-image';
 import { graphql } from 'gatsby'
-
-
 function BlogPost(props) {
     const post = props.data.markdownRemark;
-    const { title } = post.frontmatter;
+    const { title} = post.frontmatter;
     return (
         <Layout>
             <div>
                 <h1>{title}</h1>
+                <Img fluid={post.frontmatter.image.childImageSharp.fluid} />
                 <div dangerouslySetInnerHTML={{ __html: post.html }} />
             </div>
         </Layout>
     )
 }
 
-export default BlogPost;
+export default BlogPost
 export const query = graphql`
  query PostQuery($slug: String!) {
      markdownRemark(fields: { slug: { eq: $slug } }) {
@@ -25,6 +25,17 @@ export const query = graphql`
        frontmatter {
         title
         description
+        image {
+          childImageSharp {
+            resize(width: 750, height: 750) {
+              src
+            }
+            fluid(maxWidth: 786) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+       }
        }
    }
-}`
+}
+`
